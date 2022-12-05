5a1624a09382a1360ee9e368618f59ca92eb4c70
jtanila-1003 <115702581+jtanila-1003@users.noreply.github.com>
2022-12-05 09:08:04 +0800

    Update EditBookComponent.js

--- src/components/EditBooksComponent/EditBookComponent.js
+++ src/components/EditBooksComponent/EditBookComponent.js
@@ -1,5 +1,5 @@
 import { useState, useContext, useEffect } from 'react';
-import { Link, useNavigate, useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { BooksContext } from '../../store/ContextProvider';
 
 const EditBookComponent = () => {
