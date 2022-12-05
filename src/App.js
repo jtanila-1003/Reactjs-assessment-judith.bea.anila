230360c4c7d9a4ab809687f82e75defcd3d092f5
jtanila-1003 <115702581+jtanila-1003@users.noreply.github.com>
2022-12-05 09:26:49 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import AddBookComponent from './components/AddBooksComponent/AddBookComponent';
 import AppBarComponent from './components/AppBarComponents/AppBarComponent';
 import { Routes, Route } from 'react-router-dom';
-import NewBooksComponent from './components/NewBooksComponent/NewBooksComponent';
 import BookCollectionComponent from './components/BookCollectionComponent/BookCollectionComponent';
 import EditBookComponent from './components/EditBooksComponent/EditBookComponent';
 import ContextProvider from './store/ContextProvider';
@@ -14,7 +13,6 @@ function App() {
       <AppBarComponent />
       <Routes>
         <Route path="/" element={<BookCollectionComponent />} />
-        <Route path="/newbooks" element={<NewBooksComponent />} />
         <Route path="/addbooks" element={<AddBookComponent />} />
         <Route path="/editbooks/:id" element={<EditBookComponent />} />
       </Routes>
